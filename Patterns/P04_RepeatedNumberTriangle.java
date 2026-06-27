4886eee433d7110a05b68fc1f66e13f59c665495
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-27 10:36:18 +0530

    4th Pattern

--- Patterns/P04_RepeatedNumberTriangle.java
+++ Patterns/P04_RepeatedNumberTriangle.java
@@ -1,10 +1,26 @@
+//        Enter the number of rows: 5
+//        1
+//        22
+//        333
+//        4444
+//        55555
+
+import java.util.Scanner;
+
 public class P04_RepeatedNumberTriangle {
-    public static void main(String[] args) {
-        for (int row = 1; row < 5; row++) {
-            for (int col = 1; col <= row; col++) {
-                System.out.print(row + " ");
+    public static void print_repeted_number_triangle(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        print_repeted_number_triangle(rows);
     }
 }
