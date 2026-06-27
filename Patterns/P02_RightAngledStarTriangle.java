2081e0a04965763802a07c24792a066f60f269cf
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-27 08:47:32 +0530

    2nd Pattern

--- Patterns/P02_RightAngledStarTriangle.java
+++ Patterns/P02_RightAngledStarTriangle.java
@@ -1,10 +1,27 @@
+//     Enter the number of rows: 5
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+
+import java.util.Scanner;
+
 public class P02_RightAngledStarTriangle {
-    static void main(String[] args) {
-        for (int row = 0; row < 5; row++) {
-            for (int col = 0; col <= row; col++) {
+    public static void print_star_triangle(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+    }
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        print_star_triangle(rows);
+        sc.close();
+
     }
 }
