7d63733c9893feab8c273d21f213ca009439e6b0
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-27 10:28:54 +0530

    3rd Pattern

--- Patterns/P03_RightAngledNumberTriangle.java
+++ Patterns/P03_RightAngledNumberTriangle.java
@@ -1,10 +1,25 @@
+//        Enter the number of rows: 5
+//        1
+//        12
+//        123
+//        1234
+//        12345
+
+import java.util.Scanner;
+
 public class P03_RightAngledNumberTriangle {
-    static void main(String[] args) {
-        for (int row = 1; row < 5; row++) {
-            for (int col = 1; col <= row; col++) {
-                System.out.print(col + " ");
+    public static void print_number_triangle(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
+    }
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        print_number_triangle(rows);
     }
 }
