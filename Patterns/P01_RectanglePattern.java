d3490a3bec2613165d4349aee601fb2fa5d37fd3
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-27 08:31:30 +0530

    1st Pattern

--- Patterns/P01_RectanglePattern.java
+++ Patterns/P01_RectanglePattern.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class P01_RectanglePattern {
-    public static void main(String[] args) {
-        for(int row = 0;row < 5;row++){
-            for(int col = 0;col < 5;col++){
+    public static void print_rectangle_pattern(int rows, int cols) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = in.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = in.nextInt();
+
+        print_rectangle_pattern(rows, cols);
+        in.close();
     }
 }
