a247f8da25bcd30a2d03785a6189b451138bba5a
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-29 21:17:49 +0530

    Q1 done

--- 02_BasicMath/Q01_CountDigits.java
+++ 02_BasicMath/Q01_CountDigits.java
@@ -1,2 +1,26 @@
+//Enter the number: 12345
+//Number of digits in this number are: 5
+
+import java.util.Scanner;
+
 public class Q01_CountDigits {
+    public static int countDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            //int lastDigit = num % 10;
+            count += 1;
+            // System.out.println(lastDigit);
+            num = num / 10;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println("Number of digits in this number are: " + countDigits(num));
+        sc.close();
+
+    }
 }
