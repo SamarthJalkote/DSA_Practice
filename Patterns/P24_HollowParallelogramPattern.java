327f09ef2ababd37ef49d7f30c3122df3ab81ac6
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-29 10:06:08 +0530

    24th Pattern

---
+++ Patterns/P24_HollowParallelogramPattern.java
@@ -0,0 +1,34 @@
+//        Enter the number of rows: 5
+//        * * * * *
+//        *       *
+//        *       *
+//        *       *
+//        * * * * *
+
+import java.util.Scanner;
+
+public class P24_HollowParallelogramPattern {
+    public static void print_hollow_parallelogram(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        print_hollow_parallelogram(rows);
+        sc.close();
+    }
+}
