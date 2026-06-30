f1a4187adc07e420e50733d21b42eb742fedb2b6
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-30 16:19:31 +0530

    Added solutions for basic math problems

--- 02_BasicMath/Q11_EvenOdd.java
+++ 02_BasicMath/Q11_EvenOdd.java
@@ -1,2 +1,26 @@
+//Enter a number: 45
+//45 is Odd
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+import java.util.Scanner;
+
 public class Q11_EvenOdd {
+    public static String evenOdd(int a) {
+        if (a % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println(n + " is " + evenOdd(n));
+        sc.close();
+
+
+    }
 }
