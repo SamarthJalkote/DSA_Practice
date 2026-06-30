a6d52df700355c37117d189fec5ffd5ea0aea7ef
SamarthJ2005 <samjalkote111@gmail.com>
2026-06-30 16:47:55 +0530

    Added solutions for basic math problems

--- 02_BasicMath/Q12_LeapYear.java
+++ 02_BasicMath/Q12_LeapYear.java
@@ -1,2 +1,26 @@
+//Enter a year: 2026
+//2026 is not a Leap Year
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+import java.util.Scanner;
+
 public class Q12_LeapYear {
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is not a Leap Year");
+        }
+        sc.close();
+    }
 }
