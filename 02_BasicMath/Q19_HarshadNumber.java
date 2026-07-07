b9e5f812d29a30bc4db4745ea73b58a54cb7b31c
SamarthJ2005 <samjalkote111@gmail.com>
2026-07-07 22:14:26 +0530

    new question

---
+++ 02_BasicMath/Q19_HarshadNumber.java
@@ -0,0 +1,30 @@
+//A Harshad Number(Niven number) is a number that is divisible by the sum of its digits.
+
+//Enter the number : 18
+//18 is a Niven Number
+
+import java.util.*;
+
+
+public class Q19_HarshadNumber {
+
+    public static boolean print_harshad_num(int n) {
+        int originalNum = n;
+        int sum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit;
+            n = n / 10;
+        }
+        return (originalNum % sum == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        if (print_harshad_num(num)) System.out.println(num + " is a Niven Number");
+        sc.close();
+    }
+
+}
